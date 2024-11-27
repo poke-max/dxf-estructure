b9561a8cff9e4e76441c52819ef70ec05ec3a670
poke-max <javierriverfb@gmail.com>
2024-11-26 19:33:45 -0600

    d

--- app_web.py
+++ app_web.py
@@ -126,6 +126,4 @@ def generate():
         print(f"Error en generate: {str(e)}")  # Para debugging
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+app = app
